7f8f49fdd5a01b3d4584e615aa7b366464106b8c
Rishab Jain <rishab.jain@mfine.co>
2020-07-02 15:34:44 +0530

    july day 2 sol

---
+++ July30DaysCode/Binary_Tree_Level_Order_Traversal_II.js
@@ -0,0 +1,94 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function (root) {
+  var result = [];
+  var queue1 = [];
+  var queue2 = [];
+  var levelValues = [];
+  if (root) {
+    queue1.push(root);
+  }
+  while (queue1.length !== 0 || queue2.length !== 0) {
+    while (queue1.length !== 0) {
+      const element = queue1.shift();
+      levelValues.push(element.val);
+      if (element.left) {
+        queue2.push(element.left);
+      }
+      if (element.right) {
+        queue2.push(element.right);
+      }
+
+      if (queue1.length === 0) {
+        result.unshift(levelValues);
+        levelValues = [];
+      }
+    }
+
+    while (queue2.length !== 0) {
+      const element = queue2.shift();
+      levelValues.push(element.val);
+      if (element.left) {
+        queue1.push(element.left);
+      }
+      if (element.right) {
+        queue1.push(element.right);
+      }
+      if (queue2.length === 0) {
+        result.unshift(levelValues);
+        levelValues = [];
+      }
+    }
+  }
+
+  return result;
+};
+
+///////////////////////////SOLUTION 2///////////////////////////////////////
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function (root) {
+  var result = [];
+  traverseTree(root, 0, result);
+  return result.reverse();
+};
+
+function traverseTree(root, level, result) {
+  if (root === null) {
+    return;
+  }
+
+  let levelElement = result[level];
+  if (!levelElement) {
+    levelElement = [];
+    result[level] = levelElement;
+  }
+  levelElement.push(root.val);
+  if (root.left) {
+    traverseTree(root.left, level + 1, result);
+  }
+  if (root.right) {
+    traverseTree(root.right, level + 1, result);
+  }
+}
