3903e71ac8dfe9cef74b472ca517170115ee9200
Rishab1065 <rishab1065@gmail.com>
2020-10-05 12:23:22 +0530

    sol

---
+++ 100MostLikedQuestions/MedianofTwoSortedArrays.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function (nums1, nums2) {
+  nums1 = nums1.concat(nums2).sort((a, b) => a - b);
+  if (nums1.length % 2 === 0) {
+    return (nums1[nums1.length / 2] + nums1[nums1.length / 2 - 1]) / 2;
+  } else {
+    return nums1[Math.floor(nums1.length / 2)];
+  }
+};
+
+const ans = findMedianSortedArrays([-1, 3], [2]);
+console.log(ans);
